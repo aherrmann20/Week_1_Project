2889ea527564e173ab0d3e7caeccdbb995685985
aherrmann20 <aherrmann@college.harvard.edu>
2015-06-20 23:31:00 +0200

    Fixed bugs in the songs hash

--- rehearse-app.rb
+++ rehearse-app.rb
@@ -6,26 +6,25 @@ end
 
 get "/:song" do |song|
 	@songs = [
-		{"Breaking Free" =>
+		{"Breaking_Free" =>
 			{
-				:movie => "High School Musical"
-				:lyrics => "We're soarin/flyin"
+				:movie => "High School Musical",
+				:lyrics => "We're soarin/flyin",
 				:youtube_link => "youtube.com"
 			}
 		},
-		{
-			{"Can I Have this Dance"
-				:movie => "High School Musical"
-				:lyrics => "With every step we're getting better"
+		{"Can I Have this Dance" =>
+			{
+				:movie => "High School Musical",
+				:lyrics => "With every step we're getting better",
 				:youtube_link => "youtube.com"
 			}
-		},
-	
+		}
 
 	]
 	
 	@song_name = params["song"]
-	@song_artist = @songs[:artist]
+	@song_movie = @songs[:movie]
 	@song_lyrics = @songs[:lyrics]
 	@song_link = @songs[:youtube_link]
 
