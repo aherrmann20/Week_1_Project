bcc9bb798e06f928553afb8d0c50a467c4ca05f0
aherrmann20 <aherrmann@college.harvard.edu>
2015-06-20 23:22:44 +0200

    Began app

---
+++ rehearse-app.rb
@@ -0,0 +1,40 @@
+require "sinatra"
+
+get '/' do  
+	#erb :home
+end
+
+get "/:song" do |song|
+	@songs = [
+		{"Breaking Free" =>
+			{
+				:movie => "High School Musical"
+				:lyrics => "We're soarin/flyin"
+				:youtube_link => "youtube.com"
+			}
+		},
+		{
+			{"Can I Have this Dance"
+				:movie => "High School Musical"
+				:lyrics => "With every step we're getting better"
+				:youtube_link => "youtube.com"
+			}
+		},
+	
+
+	]
+	
+	@song_name = params["song"]
+	@song_artist = @songs[:artist]
+	@song_lyrics = @songs[:lyrics]
+	@song_link = @songs[:youtube_link]
+
+	erb :song
+end
+
+get "songs/:index" do  
+	erb :song_index
+end
+
+
+
