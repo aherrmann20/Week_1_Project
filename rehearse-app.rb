1077b4a0687446f88dbfc129975c21d7a0ac7610
Robert <robertlplee@gmail.com>
2015-06-21 16:06:11 +0200

    changed syntax of hashes

--- rehearse-app.rb
+++ rehearse-app.rb
@@ -1,32 +1,32 @@
 require "sinatra"
 
 get '/' do  
-	#erb :home
+	"hello!"
 end
 
+
 get "/:song" do |song|
-	@songs = [
-		{"Breaking_Free" =>
-			{
-				:movie => "High School Musical",
-				:lyrics => "We're soarin/flyin",
-				:youtube_link => "youtube.com"
-			}
+	@songs = {
+		breaking_free: {
+				movie: "High School Musical",
+				lyrics: "We're soarin/flyin",
+				youtube_link: "ZLa__49Ltv4"
 		},
-		{"Can I Have this Dance" =>
-			{
+		can_i_have_this_dance: {
 				:movie => "High School Musical",
 				:lyrics => "With every step we're getting better",
 				:youtube_link => "youtube.com"
-			}
 		}
-
-	]
+	}
 	
-	@song_name = params["song"]
-	@song_movie = @songs[:movie]
-	@song_lyrics = @songs[:lyrics]
-	@song_link = @songs[:youtube_link]
+
+	# @song_name = params["song"]
+	# @song_movie = @songs[params["song"] :movie]
+	# @song_lyrics = @songs[:lyrics]
+	# @song_link = @songs[:params[song]]
+	@chosen_song = @songs[:params[song]]
+
+	# [:youtube_link]]
 
 	erb :song
 end
